c9a2346ff8eaae9ead37176ed7c5a21cf00a8e0d
LucasLCabral <lucasludovicocabraal@gmail.com>
2025-08-23 22:59:11 -0300

    fix: s3 backend for remote tfsate implemented

--- terraform/s3_backend.tf
+++ terraform/s3_backend.tf
@@ -1,8 +1,7 @@
-# Backend S3 - Descomente após criar os recursos
-# terraform {
-#   backend "s3" {
-#     bucket = "morango-pay-terraform-state"
-#     key    = "terraform.tfstate"
-#     region = "sa-east-1"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "morango-pay-terraform-state"
+    key    = "terraform.tfstate"
+    region = "sa-east-1"
+  }
+}
